5531e10796d3ea89fd9e8f558f28e56caf43ebc6
Javier Vargas <jvargas@easysol.net>
2015-04-01 17:23:20 -0500

    Moving to more comples user schema

--- app/models/db/SystemUsers.scala
+++ app/models/db/SystemUsers.scala
@@ -1,8 +1,37 @@
 package models.db
 
-/**
- * Created by javier on 1/04/15.
- */
-class SystemUsers {
+import org.virtuslab.unicorn.LongUnicornPlay._
+import org.virtuslab.unicorn.LongUnicornPlay.driver.simple._
 
+import scala.slick.lifted.ProvenShape
+
+case class UserId(id: Long) extends AnyVal with BaseId
+
+object UserId extends IdCompanion[UserId]
+
+/** User entity.
+  */
+case class SystemUser(id: Option[UserId],
+                userId: String,
+                role: String = "guest") extends WithId[UserId]
+
+
+class SystemUsers(tag: Tag) extends IdTable[UserId, SystemUser](tag, "SYSTEM_USERS") {
+  def * : ProvenShape[SystemUser] = {
+    val shapedValue = (id.?, userId, role).shaped
+
+    shapedValue.<>({
+      tuple => SystemUser(id = tuple._1, userId = tuple._2, role = tuple._3)
+    }, {
+      (u: SystemUser) => Some {
+        (u.id, u.userId, u.role)
+      }
+    })
+  }
+
+  def userId = column[String]("userId")
+
+  def role = column[String]("role")
+
+  def userIdIdx = index("idx_userId", userId, unique = true)
 }
--- app/repositories/SystemUserRepository.scala
+++ app/repositories/SystemUserRepository.scala
@@ -1,8 +1,14 @@
 package repositories
 
-/**
- * Created by javier on 1/04/15.
- */
-object SystemUserRepository {
+import models.db.{SystemUser, SystemUsers, UserId}
+import org.virtuslab.unicorn.LongUnicornPlay._
+import org.virtuslab.unicorn.LongUnicornPlay.driver.simple._
+
+trait SystemUsersRepositories {
+  lazy val systemUsersQuery: TableQuery[SystemUsers] = TableQuery[SystemUsers]
+
+  object SystemUserRepository extends BaseIdRepository[UserId, SystemUser, SystemUsers](systemUsersQuery) {
+//https://github.com/VirtusLab/unicorn/blob/v0.6.x-slick-2.1.x/unicorn-core/src/test/scala/org/virtuslab/unicorn/repositories/UsersRepositoryTest.scala
+  }
 
 }
